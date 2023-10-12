0c7c247d43fff32bebb9e486462ea230823df336
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-10-12 13:36:37 -0400

    Adds windowing algo.

--- pkg/chaintracer/chaintracer_test.go
+++ pkg/chaintracer/chaintracer_test.go
@@ -10,11 +10,12 @@ import (
 
 func TestDataPull(t *testing.T) {
 	tracer := chaintracer.NewIncrementingTracer(18293308)
-	for ; ; tracer.IncrementBlock() {
+	for i := 0; i < 10; tracer.IncrementBlock() {
 		blockData, builder, _ := tracer.RetrieveDetails()
 		fmt.Println(blockData.Transactions[0])
 		fmt.Println(builder)
 		time.Sleep(1 * time.Second)
+		i++
 	}
 
 }
--- pkg/processors/window.go
+++ pkg/processors/window.go
@@ -1,8 +1,12 @@
 package processors
 
-import "github.com/primevprotocol/oracle/pkg/chaintracer"
+import (
+	"errors"
 
-type BidSet []string
+	"github.com/primevprotocol/oracle/pkg/chaintracer"
+)
+
+type BidSet map[string]struct{}
 
 type Processor interface {
 	ProcessBlock(block chaintracer.BlockDetails, openBids BidSet) (open BidSet, closed BidSet, err error)
@@ -12,9 +16,24 @@ type WindowAlgo struct{}
 
 // ProcessBlock is not aware of block builder dynamics, it is simply passed a block and a set of open bids, and told to figure out which bids are closed.
 func (w WindowAlgo) ProcessBlock(block chaintracer.BlockDetails, openBids BidSet) (open BidSet, closed BidSet, err error) {
-	for txn := range block.Transactions {
-		// Process
-		_ = txn
+
+	if block.Transactions == nil {
+		return nil, nil, errors.New("transactions cannot be nil")
 	}
-	return open, closed, nil
+
+	// Initialize sets
+	closedBids := make(BidSet)
+
+	for _, txn := range block.Transactions {
+		if _, exists := openBids[txn]; exists {
+			closedBids[txn] = struct{}{}
+		}
+	}
+
+	// Remove closed bids from the open set
+	for txn := range closedBids {
+		delete(openBids, txn)
+	}
+
+	return openBids, closedBids, nil
 }

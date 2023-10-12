783f6a7f543982fc8c94c329e4d3fb9077937be6
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-10-12 13:39:38 -0400

    Adds simpler test for winnign builder.

--- pkg/chaintracer/chaintracer_test.go
+++ pkg/chaintracer/chaintracer_test.go
@@ -1,21 +1,18 @@
 package chaintracer_test
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
-	"time"
 
 	"github.com/primevprotocol/oracle/pkg/chaintracer"
 )
 
 func TestDataPull(t *testing.T) {
 	tracer := chaintracer.NewIncrementingTracer(18293308)
-	for i := 0; i < 10; tracer.IncrementBlock() {
-		blockData, builder, _ := tracer.RetrieveDetails()
-		fmt.Println(blockData.Transactions[0])
-		fmt.Println(builder)
-		time.Sleep(1 * time.Second)
-		i++
+	_, builder, _ := tracer.RetrieveDetails()
+
+	if !reflect.DeepEqual("titanbuilder", builder) {
+		t.Error("winning builder is not titanbuilder for block 18293308")
 	}
 
 }

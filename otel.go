af05c4b6e45fd4f237aac68327ae2e59276e3980
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-12-02 18:49:06 -0500

    Adds exporter for otel.

--- otel.go
+++ otel.go
@@ -6,14 +6,19 @@ import (
 	"time"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
+
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
+
+func newExporter(ctx context.Context) (trace.SpanExporter, error) {
+	return otlptracegrpc.New(ctx)
+}
 
 // setupOTelSDK bootstraps the OpenTelemetry pipeline.
 // If it does not return an error, make sure to call shutdown for proper cleanup.
@@ -49,7 +54,7 @@ func setupOTelSDK(ctx context.Context, serviceName, serviceVersion string) (shut
 	otel.SetTextMapPropagator(prop)
 
 	// Set up trace provider.
-	tracerProvider, err := newTraceProvider(res)
+	tracerProvider, err := newTraceProvider(ctx, res, serviceName, serviceVersion)
 	if err != nil {
 		handleErr(err)
 		return
@@ -65,6 +70,9 @@ func setupOTelSDK(ctx context.Context, serviceName, serviceVersion string) (shut
 	}
 	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
 	otel.SetMeterProvider(meterProvider)
+
+	// Append to shutdown functions
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
 
 	return
 }
@@ -84,20 +92,24 @@ func newPropagator() propagation.TextMapPropagator {
 	)
 }
 
-func newTraceProvider(res *resource.Resource) (*trace.TracerProvider, error) {
-	traceExporter, err := stdouttrace.New(
-		stdouttrace.WithPrettyPrint())
+func newTraceProvider(ctx context.Context, res *resource.Resource, serviceName string, serviceVersion string) (*trace.TracerProvider, error) {
+
+	// Configure OTLP Exporter
+	exp, err := newExporter(ctx)
 	if err != nil {
 		return nil, err
 	}
-
-	traceProvider := trace.NewTracerProvider(
-		trace.WithBatcher(traceExporter,
-			// Default is 5s. Set to 1s for demonstrative purposes.
-			trace.WithBatchTimeout(time.Second)),
-		trace.WithResource(res),
+	// Set up trace provider with OTLP exporter
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+			semconv.ServiceVersion(serviceVersion),
+		)),
 	)
-	return traceProvider, nil
+
+	return tracerProvider, nil
 }
 
 func newMeterProvider(res *resource.Resource) (*metric.MeterProvider, error) {

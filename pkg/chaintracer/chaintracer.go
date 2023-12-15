6f454698d2d7eb5d30b5132e835af9efd9511246
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-12-14 19:37:58 -0500

    Removes unused experiemental code.

--- pkg/chaintracer/chaintracer.go
+++ pkg/chaintracer/chaintracer.go
@@ -1,19 +1,14 @@
 package chaintracer
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"math/big"
 
-	"io"
-	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/pkg/errors"
-	"github.com/primevprotocol/oracle/pkg/repository"
 	"github.com/primevprotocol/oracle/pkg/rollupclient"
 	"github.com/rs/zerolog/log"
 )
@@ -50,6 +45,7 @@ func (st *SmartContractTracer) GetNextBlockNumber(ctx context.Context) (NewBlock
 		return st.currentBlockNumberCached
 	}
 
+	// TODO(@ckartik): Use stored block number on contract instead of incrementing (For Failure reslience)
 	// nextBlockNumber, err := st.contractClient.GetNextRequestedBlockNumber(&bind.CallOpts{
 	// 	Pending: false,
 	// 	From:    common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
@@ -128,25 +124,10 @@ func NewSmartContractTracer(ctx context.Context, contractClient *rollupclient.Ro
 
 }
 
-/*
-Redundant Testing Code Begins here
-*/
 type L1DataRetriver interface {
 	GetTransactions(blockNumber int64) (*BlockDetails, error)
 	GetWinningBuilder(blockNumber int64) (string, error)
 	GetLatestBlockNumber() (int64, error)
-}
-
-func NewIncrementingTracer(startingBlockNumber int64, rateLimit time.Duration) Tracer {
-	return &IncrementingTracer{
-		BlockNumber: startingBlockNumber,
-		RateLimit:   rateLimit,
-	}
-}
-
-type IncrementingTracer struct {
-	BlockNumber int64
-	RateLimit   time.Duration
 }
 
 type Tracer interface {
@@ -159,209 +140,4 @@ type Payload struct {
 	Method  string        `json:"method"`
 	Params  []interface{} `json:"params"`
 	ID      int           `json:"id"`
-}
-
-func (it *IncrementingTracer) GetNextBlockNumber(_ context.Context) (NewBlockNumber int64) {
-	it.BlockNumber += 1
-	log.Info().Int64("BlockNumber", it.BlockNumber).Msg("Incremented Block Number")
-	return it.BlockNumber
-}
-
-func (it *IncrementingTracer) RetrieveDetails() (block *BlockDetails, BlockBuilder string, err error) {
-	log.Info().Str("RateLimit", it.RateLimit.String()).Msg("Waiting for Rate Limit")
-	time.Sleep(it.RateLimit)
-	retries := 0
-	var blockData *BlockDetails
-	log.Debug().Msg("Starting Retreival of Block Details")
-	// Retrieve Block Details from Infura
-	for blockData == nil {
-		log.Debug().Msg("fetching infura data")
-		blockData = InfuraData(it.BlockNumber, "https://mainnet.infura.io/v3/b8877b173a0543bea7dca82c313e7347")
-		time.Sleep(time.Duration(retries*2) * time.Second)
-		if retries > 5 {
-			log.Error().Msg("Error: Could not retrieve block data")
-			return nil, "", errors.New("Error: Could not retrieve block data")
-		}
-		retries += 1
-	}
-	retries = 0
-	var builderName string
-	for builderName == "" {
-		log.Debug().Msg("fetching Payloadsde data")
-		builderName, err = PayloadsDe(it.BlockNumber)
-		if err != nil {
-			log.Error().Err(err).Msg("Error: Could not retrieve block data")
-		}
-		time.Sleep(time.Duration(retries*2) * time.Second)
-		if retries > 5 {
-			return nil, "", errors.New("Error: Could not retrieve block data")
-		}
-		retries += 1
-	}
-	log.Info().
-		Int64("BlockNumber", it.BlockNumber).
-		Str("Builder", builderName).
-		Int("Txns Received", len(blockData.Transactions)).
-		Msg("Finished Retreival of Block Details")
-
-	return blockData, builderName, nil
-}
-
-type MainnetInfuraAndPayloadsdeDataRetriever struct {
-	url string
-}
-
-func (st *MainnetInfuraAndPayloadsdeDataRetriever) GetLatestBlockNumber() (int64, error) {
-	return 0, errors.New("not implemented")
-}
-
-func NewMainnetInfuraAndPayloadsdeDataRetriever() L1DataRetriver {
-	return &MainnetInfuraAndPayloadsdeDataRetriever{
-		url: "https://mainnet.infura.io/v3/b8877b173a0543bea7dca82c313e7347",
-	}
-}
-
-func (m MainnetInfuraAndPayloadsdeDataRetriever) GetTransactions(blockNumber int64) (*BlockDetails, error) {
-	return InfuraData(blockNumber, m.url), nil
-}
-
-func (m MainnetInfuraAndPayloadsdeDataRetriever) GetWinningBuilder(blockNumber int64) (string, error) {
-	return PayloadsDe(blockNumber)
-}
-
-func PayloadsDe(blockNumber int64) (string, error) {
-	url := "https://api.payload.de/block_info?block=" + strconv.FormatInt(blockNumber, 10)
-	resp, err := http.Get(url)
-	log.Debug().Msg("payloadsde data recieved")
-	if err != nil {
-		log.Error().Err(err).Str("URL", url).Msg("Error sending request")
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Error().Err(err).Msg("Error reading response")
-		return "", err
-	}
-
-	var payload struct {
-		Builder string `json:"builder"`
-	}
-	json.Unmarshal(data, &payload)
-
-	return payload.Builder, nil
-}
-
-func InfuraData(blockNumber int64, url string) *BlockDetails {
-	hex := strconv.FormatInt(blockNumber, 16)
-	// url := "https://mainnet.infura.io/v3/b8877b173a0543bea7dca82c313e7347"
-	payload := Payload{
-		Jsonrpc: "2.0",
-		Method:  "eth_getBlockByNumber",
-		Params:  []interface{}{"0x" + hex, false},
-		ID:      1,
-	}
-	payloadBytes, err := json.Marshal(payload)
-	if err != nil {
-		log.Error().Err(err).Msg("Error marshalling payload")
-		return nil
-	}
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
-	if err != nil {
-		log.Error().Err(err).Msg("Error creating request")
-		return nil
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth("", "66f7989079d54ad6988e3b083da0723a")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Error().Err(err).Msg("Error sending request")
-		return nil
-	}
-	defer resp.Body.Close()
-
-	var infuraresp struct {
-		Result BlockDetails `json:"result"`
-	}
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Error().Err(err).Msg("Error reading response")
-		return nil
-	}
-	json.Unmarshal(data, &infuraresp)
-	if infuraresp.Result.BlockNumber == "" {
-		log.Error().Msg("Error: Block data is empty")
-		return nil
-	}
-	return &infuraresp.Result
-}
-
-func NewIntegrationTestTracer(ctx context.Context, contractClient *rollupclient.Rollupclient, cs repository.CommitmentsStore) Tracer {
-	tracer := &IntegerationTestTracer{
-		contractClient: contractClient,
-		cs:             cs,
-	}
-	tracer.GetNextBlockNumber(ctx)
-	return tracer
-}
-
-func (st *IntegerationTestTracer) GetNextBlockNumber(ctx context.Context) (NewBlockNumber int64) {
-	// nextBlockNumber, err := st.contractClient.GetNextRequestedBlockNumber(&bind.CallOpts{
-	// 	Pending: false,
-	// 	From:    common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
-	// 	Context: ctx,
-	// })
-	// for err != nil {
-	// 	select {
-	// 	case <-ctx.Done():
-	// 		log.Info().Msg("Context cancelled, exiting GetNextBlockNumber")
-	// 		return -1
-	// 	default:
-	// 		log.Error().Err(err).Msg("Error getting next block number, will go to sleep for 5 seconds and try again")
-	// 		time.Sleep(5 * time.Second)
-	// 		nextBlockNumber, err = st.contractClient.GetNextRequestedBlockNumber(&bind.CallOpts{
-	// 			Pending: false,
-	// 			From:    common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
-	// 			Context: ctx,
-	// 		})
-	// 	}
-	// }
-	st.currentBlockNumberCached += 1
-	return st.currentBlockNumberCached
-}
-
-// TODO(@ckartik): Move logic for service based data request to an isolated function.
-func (it *IntegerationTestTracer) RetrieveDetails() (block *BlockDetails, BlockBuilder string, err error) {
-	txns, err := it.cs.RetrieveCommitments(it.currentBlockNumberCached)
-	if err != nil {
-		log.Error().Err(err).Msg("Error retrieving txns in integreation test tracer")
-		return nil, "", err
-	}
-
-	blockData := &BlockDetails{
-		BlockNumber:  strconv.FormatInt(it.currentBlockNumberCached, 10),
-		Transactions: []string{},
-	}
-
-	for txn := range txns {
-		time.Sleep(50 * time.Millisecond)
-		blockData.Transactions = append(blockData.Transactions, txn)
-	}
-	return blockData, "dummy builder", nil
-}
-
-// IntegerationTestTracer is a tracer that uses the smart contract
-// to retrieve details about the next block that needs to be proccesed
-// it has the option of
-type IntegerationTestTracer struct {
-	contractClient           *rollupclient.Rollupclient
-	currentBlockNumberCached int64
-	cs                       repository.CommitmentsStore
-
-	RateLimit time.Duration
 }
--- pkg/chaintracer/chaintracer_test.go
+++ pkg/chaintracer/chaintracer_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	repository "github.com/primevprotocol/oracle/pkg/Repository"
 	"github.com/primevprotocol/oracle/pkg/chaintracer"
 	"github.com/stretchr/testify/assert"
 )
@@ -45,50 +46,6 @@ func getAuth(privateKey *ecdsa.PrivateKey, chainID *big.Int, client *ethclient.C
 
 	return auth
 }
-
-/*
-func TestDataPull(t *testing.T) {
-	CONTRACT_ADDRESS := "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
-	CLIENT_URL := "http://localhost:8545"
-
-	client, err := ethclient.Dial(CLIENT_URL)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
-	}
-
-	tracer := chaintracer.NewIncrementingTracer(18293308)
-	_, builder, err := tracer.RetrieveDetails()
-
-	if !reflect.DeepEqual("titanbuilder", builder) {
-		t.Error("winning builder is not titanbuilder for block 18293308")
-	}
-	if err != nil {
-		t.Error("error retrieving block details")
-	}
-	rc, err := rollupclient.NewClient(common.HexToAddress(CONTRACT_ADDRESS), client)
-	if err != nil {
-		t.Error("error creating rollup client")
-	}
-	CHAIN_ID := big.NewInt(31337)
-
-	privateKey, err := crypto.HexToECDSA("ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
-	if err != nil {
-		t.Error("error creating private key")
-	}
-
-	txn, err := rc.AddBuilderAddress(getAuth(privateKey, CHAIN_ID, client, t), "k builder", common.HexToAddress("0x5FbDB2315678afecb367f032d93F642f64180aa3"))
-	if err != nil {
-		t.Error("error adding builder address")
-	}
-	t.Log(txn.Hash().String())
-
-	txn2, err := rc.ReceiveBlockData(getAuth(privateKey, CHAIN_ID, client, t), []string{"txn1", "txn2", "txn3"}, big.NewInt(2000), "k builder")
-	if err != nil {
-		t.Fatalf("error on recieve block data %v", err)
-	}
-	t.Log(txn2.Hash().String())
-}
-*/
 
 type PreConfirmationsContract interface {
 	GetCommitmentsByBlockNumber(opts *bind.CallOpts, blockNumber *big.Int) ([][32]byte, error)
@@ -147,7 +104,7 @@ func (d *mockTracer) RetrieveDetails() (block *chaintracer.BlockDetails, BlockBu
 }
 
 func TestFilter(t *testing.T) {
-	txnFilter := chaintracer.NewTransactionCommitmentFilter(&mockPreConfContract{
+	txnFilter := repository.NewTransactionCommitmentFilter(&mockPreConfContract{
 		commitmentsAndTxnHashes: map[[32]byte]string{
 			{0x02, 0x90, 0x00}: "0xalok",
 			{0x02, 0x90, 0x01}: "0xkartik",
